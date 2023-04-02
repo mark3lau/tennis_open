bdd953daefc926cd95820e1ed8eb8658bd362508
mark3lau <mark3lau@gmail.com>
2023-04-02 07:12:25 +0000

    added package views and template

--- tennis_lessons/urls.py
+++ tennis_lessons/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.tennis_fitness_packages, name='packages'),
 ]
--- tennis_lessons/views.py
+++ tennis_lessons/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Package
 
-# Create your views here.
+
+def tennis_fitness_packages(request):
+    """ A view to show all tennis and fitness packages """
+
+    packages = Package.objects.all()
+
+    context = {
+        'packages': packages,
+    }
+    
+    return render(request, 'packages/packages.html', context)
