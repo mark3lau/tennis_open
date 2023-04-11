14c76a2ca0fa7c517206639c73f3c4c890f59efb
mark3lau <mark3lau@gmail.com>
2023-04-11 21:01:24 +0000

    added toasts html into includes folder and javascript code to the remove function

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 from tennis_lessons.models import Package
 from django.views.decorators.http import require_POST
 
@@ -6,11 +7,14 @@ from django.views.decorators.http import require_POST
 def add_to_bag(request, package_id):
     """ Add a package to the shopping bag """
 
+    package = Package.objects.get(pk=package_id)
     bag = request.session.get('bag', {})
     if package_id in bag:
         bag[package_id] += 1
     else:
         bag[package_id] = 1
+        messages.success(request, f'Added {package.name} to your bag')
+
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
@@ -19,3 +23,14 @@ def view_bag(request):
     """ A view to render the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def remove_from_bag(request, package_id):
+    """ Remove an item from the bag """
+
+    bag = request.session.get('bag', {})
+    if package_id in bag:
+        bag.pop(package_id)
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
