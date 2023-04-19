bbf5573a7cc8d0fcacfede799545fd36a3ebcd15
mark3lau <mark3lau@gmail.com>
2023-04-19 11:30:19 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -7,12 +7,13 @@ def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag")
-        return redirect(reverse('packages'))
+        return redirect(reverse('checkout'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MyYnbBPaZVYK7rUSwxbvbS9lj3FSzjhdDE8WnchAn4rozZQoyU17gheBq6vizMYbSEqrtLMxOFOdmOf88i1F35S00KusiS5U6',
+        'client_secret': 'test client secret',
     }
-
     return render(request, template, context)
