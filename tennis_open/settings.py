3c85e782ddd80162f0d9b9608d8934817c685304
mark3lau <mark3lau@gmail.com>
2023-07-05 20:07:21 +0000

    update debug to false

--- tennis_open/settings.py
+++ tennis_open/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'tennis-open.herokuapp.com', '8000-mark3lau-tennisopen-i4m1w0jrsqn.ws-eu101.gitpod.io']
 
